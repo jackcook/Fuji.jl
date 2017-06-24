4801c9cabce59aaa9dcaa53415f6aec50b60ba13
Jack Cook <hello@jackcook.nyc>
2017-06-24 13:16:21 -0700

    Make host and port configurable

--- src/Fuji.jl
+++ src/Fuji.jl
@@ -21,7 +21,7 @@ function route!(action::Function, server::FujiServer, path::AbstractString)
     push!(server.routes, route)
 end
 
-function start(server::FujiServer)
+function start(server::FujiServer, host=IPv4(127, 0, 0, 1), port=8000)
     http = HttpHandler() do req, res
         timestamp = Dates.format(now(), "u d, yyyy HH:MM:SS")
 
@@ -38,12 +38,12 @@ function start(server::FujiServer)
         response
     end
 
-    http.events["listen"] = (saddr) -> println(" * Running on http://$saddr/ (Press CTRL+C to quit)")
+    http.events["listen"] = (saddr) -> log(" * Running on http://$saddr/ (Press CTRL+C to quit)")
 
     web_server = Server(http)
 
     try
-        run(web_server, host=IPv4(127, 0, 0, 1), port=8000)
+        run(web_server, host=host, port=port)
     catch e
         if isa(e, InterruptException)
             println("Stopping server...")
