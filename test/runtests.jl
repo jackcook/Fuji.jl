b88e928648d00061a59297afa75f4bf728a8e080
Jack Cook <hello@jackcook.nyc>
2017-06-24 13:44:41 -0700

    Wrote unit tests and configured Travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
-using Fuji
 using Base.Test
+using Fuji
+using Requests
 
-# write your own tests here
-@test 1 == 2
+@testset begin
+    server = FujiServer()
+
+    @test size(server.routes, 1) == 0
+
+    route!(server, "/hello") do
+        "hi"
+    end
+
+    @test size(server.routes, 1) == 1
+    @test server.routes[1].path == "/hello"
+    @test server.routes[1].action() == "hi"
+
+    @spawn Fuji.start(server)
+
+    @test Requests.get("http://localhost:8000").status == 404
+    @test Requests.get("http://localhost:8000/hello").status == 200
+end
