c776307ecd73d7becb2bff6503b730bdbf47e1d9
Jack Cook <hello@jackcook.nyc>
2017-06-24 17:40:08 -0700

    Added splat param parsing

---
+++ src/route.jl
@@ -0,0 +1,17 @@
+import Base.ismatch
+
+immutable Route
+    action::Function
+    endpoint::AbstractString
+end
+
+function ismatch(route::Route, request::Request)
+    splat_param_regex = r":[A-z0-9\-_]+"
+
+    if ismatch(splat_param_regex, route.endpoint)
+        regex_endpoint = Regex(string("^", replace(route.endpoint, splat_param_regex, "([A-z0-9\-_]+)"), "\$"))
+        ismatch(regex_endpoint, request.resource)
+    else
+        route.endpoint == request.resource
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,21 +2,36 @@ using Base.Test
 using Fuji
 using Requests
 
-@testset begin
-    server = FujiServer()
+req(endpoint) = Requests.get("http://localhost:8000$endpoint")
 
+server = FujiServer()
+@spawn Fuji.start(server)
+
+@testset "simple route" begin
     @test size(server.routes, 1) == 0
 
-    route!(server, "/hello") do
+    route!(server, "/hi") do
         "hi"
     end
 
     @test size(server.routes, 1) == 1
-    @test server.routes[1].path == "/hello"
+    @test server.routes[1].endpoint == "/hi"
     @test server.routes[1].action() == "hi"
 
-    @spawn Fuji.start(server)
+    @test req("").status == 404
+    @test req("/hi").status == 200
+end
 
-    @test Requests.get("http://localhost:8000").status == 404
-    @test Requests.get("http://localhost:8000/hello").status == 200
+@testset "splat param" begin
+    route!(server, "/hello/:name") do
+        "hello!"
+    end
+
+    @test Requests.get("http://localhost:8000/hello/:name").status == 404
+    @test Requests.get("http://localhost:8000/hello").status == 404
+    @test Requests.get("http://localhost:8000/hello/jack/asdf").status == 404
+    @test Requests.get("http://localhost:8000/hello/ja;ck").status == 404
+
+    @test Requests.get("http://localhost:8000/hello/jack").status == 200
+    @test Requests.get("http://localhost:8000/hello/j-a_ck0-1").status == 200
 end
