087d081ed17ed300939859e264d3d4ed7c9e4d1c
autodocs <autodocs>
2017-06-28 00:08:40 +0000

    build based on 048b624

---
+++ latest/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Fuji.jl",
+    "title": "Fuji.jl",
+    "category": "page",
+    "text": "Author = \"Jack Cook\""
+},
+
+{
+    "location": "index.html#Fuji.jl-1",
+    "page": "Fuji.jl",
+    "title": "Fuji.jl",
+    "category": "section",
+    "text": "Fuji is a lightweight web framework written in Julia. It allows you to quickly build and iterate on web applications with relatively few lines of code."
+},
+
+{
+    "location": "index.html#Quickstart-1",
+    "page": "Fuji.jl",
+    "title": "Quickstart",
+    "category": "section",
+    "text": "using Fuji\n\nroute(\"/hello\") do req, res\n    \"Hello, world!\"\nend\n\nFuji.start()"
+},
+
+{
+    "location": "index.html#See-the-result-1",
+    "page": "Fuji.jl",
+    "title": "See the result",
+    "category": "section",
+    "text": "http://localhost:8000/hello"
+},
+
+{
+    "location": "index.html#Built-for-productivity-1",
+    "page": "Fuji.jl",
+    "title": "Built for productivity",
+    "category": "section",
+    "text": "Fuji allows you to get your next website/REST API up and running with minimal boilerplate code forced upon you by many modern web frameworks. It also comes with the benefit of running on Julia, one of the best up-and-coming programming languages available. With over 1,000+ packages and a thriving community, Julia serves as a great option for your next project."
+},
+
+{
+    "location": "index.html#Contribute-1",
+    "page": "Fuji.jl",
+    "title": "Contribute",
+    "category": "section",
+    "text": "Found a bug? Have a good idea for improving Fuji? Please feel free to create an issue or pull request on our GitHub repo. We are a young project, and will always be welcoming to new ideas and contributions."
+},
+
+]}
